4b96cb75e482ebaf22758199cb04fada99dccdf5
dohertre <dohertre@rose-hulman.edu>
2019-08-02 16:56:40 -0400

    robot

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -8,8 +8,25 @@ Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
 import tkinter
 from tkinter import ttk
 
+
 def magic():
     print('Magic')
+
+
+def check_for_ok(entry_box):
+    contents = entry_box.get()
+    if contents == 'ok':
+        print('Hello')
+    else:
+        print('Goodbye')
+
+
+def print_n_times(entry_box, entry_box_2):
+    s = entry_box_2.get()
+    n = int(s)
+    contents = entry_box.get()
+    for k in range(n):
+        print(contents)
 
 
 def main():
@@ -40,16 +57,21 @@ def main():
     # ------------------------------------------------------------------
     magic_button['command'] = lambda: magic()
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+    my_entry_box = ttk.Entry(frame1)
+    my_entry_box.grid()
 
+    hello_goodbye_button = ttk.Button(frame1, text='Test for ok')
+    hello_goodbye_button['command'] = lambda: check_for_ok(my_entry_box)
+    hello_goodbye_button.grid()
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -71,7 +93,12 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    my_entry_box_2 = ttk.Entry(frame1)
+    my_entry_box_2.grid()
 
+    print_n_times_button = ttk.Button(frame1, text='Print Many Times')
+    print_n_times_button['command'] = lambda: print_n_times(my_entry_box, my_entry_box_2)
+    print_n_times_button.grid()
     # ------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
